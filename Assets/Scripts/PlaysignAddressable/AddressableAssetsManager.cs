15078cf04465ba552239b0d5ccc4f616ab253d99
Toni Alatalo <toni@playsign.net>
2019-09-24 15:30:18 +0300

    wip actual model. couple new targets.

---
+++ Assets/Scripts/PlaysignAddressable/AddressableAssetsManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class AddressableAssetsManager : MonoBehaviour
+{
+    public static Action Completed;
+
+    void Start()
+    {
+        AsyncOperationHandle catalogLoad = Addressables.LoadContentCatalogAsync("https://storage.googleapis.com/sites.playsign.net/warjakka-assets2/StandaloneWindows64/catalog_1.json");
+        catalogLoad.Completed += CatalogLoad_Completed;
+    }
+
+    private void CatalogLoad_Completed(AsyncOperationHandle obj)
+    {
+        Completed();
+    }
+ }
---
+++ Assets/Scripts/PlaysignAddressable/LoadAddressableObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class LoadAddressableObject : MonoBehaviour
+{
+    /* when AddressableAssetsManager is used for external catalog loading */
+    private void Awake()
+    {
+        AddressableAssetsManager.Completed += LoadObject;
+    }
+
+    //manually normally set catalog url in inspector
+    //void Start()
+    //{
+    //    LoadObject();
+    //}
+    
+    void LoadObject()
+    {
+        Debug.Log("[LoadAddressableObject] - LoadObject");
+        Addressables.InstantiateAsync("varjakka2"); //, randSpot, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
